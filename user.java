abcc92a7c152878f7286bdcf1b5b4b1b71495e97
Manuel Pinto <manuel11.pinto11@gmail.com>
2017-10-05 16:03:39 +0100

    Update user.java

--- user.java
+++ user.java
@@ -1,18 +1,40 @@
-import java.io.*;
-import java.net.*;
+import java.net.*; 
+import java.io.*; 
+import java.lang.*;
 
-class TCPClient {
- public static void main(String args[]) throws Exception {
-        String sentence;
-        String modifiedSentence;
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("127.0.0.1", 6789);
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        sentence = inFromUser.readLine();
-        outToServer.writeBytes(sentence + "\n");
-        modifiedSentence = inFromServer.readLine();
-        System.out.println("FROM SERVER:" + modifiedSentence);
-        clientSocket.close();
-    }
+public class User { 
+	public static void main (String args[]) 
+	{
+		Socket s = null; 
+		try{ 
+		  	int serverPort = 58011;
+			String ip = "tejo";
+			String data = "LST\n"; 
+		  	s = new Socket(ip, serverPort); 
+		  	DataOutputStream output = new DataOutputStream(s.getOutputStream());
+		  	byte[] arrayOutput = data.getBytes();
+		  	output.write(arrayOutput);
+		  	DataInputStream input = new DataInputStream( s.getInputStream()); 
+		  	BufferedReader buffInput = new BufferedReader(new InputStreamReader(input));
+		  	StringBuffer sb = new StringBuffer();
+			String st = "";
+			while((st = buffInput.readLine()) != null) {
+    			sb.append(st);
+			}
+			String dt = sb.toString();
+			System.out.println(dt);
+		}
+		catch (UnknownHostException e){ 
+			System.out.println("Sock:"+e.getMessage());}
+		catch (EOFException e){
+			System.out.println("EOF:"+e.getMessage()); }
+		catch (IOException e){
+			System.out.println("IO:"+e.getMessage());} 
+		finally {
+			  if(s!=null) 
+				  try {s.close();
+				  } 
+				  catch (IOException e) {/*close failed*/}
+	}
+  }
 }
